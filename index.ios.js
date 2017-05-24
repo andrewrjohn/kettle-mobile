17e41f5afc6eab1e800b2ae1dec02cb7e375a50f
Andrew <ajlutheran@gmail.com>
2017-05-23 20:33:38 -0500

    Can add 'notes' to Firebase

---
+++ index.ios.js
@@ -0,0 +1,131 @@
+/**
+ * Sample Firebase & React Native App
+ * https://github.com/davideast/firebase-react-native-sample
+ */
+'use strict';
+
+import React, {Component} from 'react';
+import ReactNative from 'react-native';
+const firebase = require('firebase');
+
+// External components
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js')
+
+const {
+  AppRegistry,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  AlertIOS,
+} = ReactNative;
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD71FqS5lCiGdJuE8UrfS4Ic_TgHsgikV4",
+    authDomain: "kettle-84ea2.firebaseapp.com",
+    databaseURL: "https://kettle-84ea2.firebaseio.com",
+    projectId: "kettle-84ea2",
+    storageBucket: "kettle-84ea2.appspot.com"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+class kettle extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('items');
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={styles.listview}/>
+
+        <ActionButton onPress={this._addItem.bind(this)} title="Add" />
+
+      </View>
+    )
+  }
+
+  _addItem() {
+    AlertIOS.prompt(
+      'Add New Item',
+      null,
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+      'plain-text'
+    );
+  }
+
+  _renderItem(item) {
+
+    /*const onPress = () => {
+      AlertIOS.alert(
+        'Complete',
+        null,
+        [
+          {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancelled')}
+        ]
+      );
+    };*/
+
+    return (
+      <ListItem item={item}/>
+    );
+  }
+
+}
+
+AppRegistry.registerComponent('kettle', () => kettle);
